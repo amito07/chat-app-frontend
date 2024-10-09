ced223147602e323e6c3fd1aed7ec3787e17520b
mandalamit171 <amit.mandal@v2.ltd>
2024-10-10 00:28:05 +0600

    feat: add react-query in the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,26 @@ import Home from "./Pages/Home";
 import Chat from "./Pages/Chat";
 import Navbar from "./Components/Navbar";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <Navbar />
       <div className="main-content">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" exact element={<Home />} />
-          <Route path="/chats" element={<Chat />} />
-          {/* <PrivateRoute path="/admin" component={Admin} /> */}
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="/chats" element={<Chat />} />
+            {/* <PrivateRoute path="/admin" component={Admin} /> */}
+          </Routes>
+          <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
       </div>
     </>
   );
